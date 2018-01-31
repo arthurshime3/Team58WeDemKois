0e696f22d0ef95880fd170a68c13fd7b947c8fe7
Jeffrey Minowa <jeffminowa@gatech.edu>
2018-01-31 00:16:04 -0500

    Auto stash before merge of "Jeffrey" and "master"

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,11 +1,12 @@
 package edu.gatech.oad.antlab.person;
+import java.util.*;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author Sam Loop
  * @version 1.1
  */
 public class Person2 {
@@ -30,8 +31,12 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+		String returnThis = "";
+		List<String> letters = Arrays.asList(input.split(""));
+  		for (String letter : letters) {
+    		returnThis += letter;
+  		}
+  		return returnThis;
 	}
 	/**
 	 * Return a string rep of this object
--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -57,24 +57,24 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("P1");
-		  toPrint += p1.toString("gburdell1");
+		  Person1 p1 = new Person1("Arthur");
+		  toPrint += p1.toString("903212700");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("gburdell2");
+		  Person2 p2 = new Person2("Sam Loop");
+		  toPrint += p2.toString("903138329");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("903035622");
+		  toPrint += p3.toString("jminowa3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+                  Person4 p4 = new Person4("Miguel");
+                  toPrint += p4.toString("mbclr3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Conor Brownell");
+                  toPrint += p5.toString("903117476");
 		  
         System.out.println(toPrint);
         
